96656c27ed07ef15091fdffeeb2ce5ee098ec6d0
aravind <aravindlakshmanan007@gmail.com>
2018-06-20 17:22:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aravind/awedi/local.java
@@ -0,0 +1,13 @@
+package com.example.aravind.awedi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class local extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local);
+    }
+}
